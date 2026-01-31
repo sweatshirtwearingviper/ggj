64c857202e57c6eb83986fa69925ca929da90a2a
sweatshirtwearingviper <thesweateredviper@gmail.com>
2026-01-31 12:21:00 -0700

    green: add double jump

--- player_controller/player_easy_control/player_easy_control.gd
+++ player_controller/player_easy_control/player_easy_control.gd
@@ -4,6 +4,8 @@ class_name PlayerEasyControl
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+var double_jumped:bool = false
+
 @onready var sprite:Sprite2D = $Character
 
 
@@ -21,10 +23,17 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	else:
+		# Reset double jump when on ground
+		double_jumped = false
 
 	# Handle jump.
-	if Input.is_action_just_pressed("up") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("up"):
+		if Global.current_colors[Global.Colors.GREEN]:
+			velocity.y = JUMP_VELOCITY
+			double_jumped = true
+		if is_on_floor():
+			velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
