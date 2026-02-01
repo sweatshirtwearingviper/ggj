d433510a23cd2d6987f546d86730c9a5aef9a06d
sweatshirtwearingviper <thesweateredviper@gmail.com>
2026-01-31 18:29:13 -0700

    fix maskbar on gravestone reset

--- ui/mask_bar/mask_bar.gd
+++ ui/mask_bar/mask_bar.gd
@@ -4,6 +4,14 @@ var is_shaking:bool = false
 var flip:bool = false
 
 
+func _ready() -> void:
+	Global.color_changed.connect(color_changed)
+	Global.color_unlocked.connect(colors_unlocked)
+	Global.color_blocked.connect(color_blocked)
+	Global.colors_cleared.connect(colors_cleared)
+	colors_cleared()
+
+
 func _physics_process(_delta) -> void:
 	if is_shaking and Engine.get_physics_frames() % 2 == 0:
 		if flip:
@@ -12,16 +20,6 @@ func _physics_process(_delta) -> void:
 			position.x -= 2
 		flip = false if flip else true
 		get_tree().create_timer(0.5).timeout.connect(func() -> void: position.x = 0; is_shaking = false)
-
-
-func _ready() -> void:
-	Global.color_changed.connect(color_changed)
-	Global.color_unlocked.connect(colors_unlocked)
-	Global.color_blocked.connect(color_blocked)
-	for child:TextureRect in $Blackouts.get_children():
-		child.modulate = Color.BLACK
-	for child:TextureRect in $Highlights.get_children():
-		child.modulate = Color(1,1,1,0)
 
 
 func color_changed() -> void:
@@ -47,3 +45,9 @@ func colors_unlocked() -> void:
 func color_blocked() -> void:
 	is_shaking = true
 	
+
+func colors_cleared() -> void:
+	for child:TextureRect in $Blackouts.get_children():
+		child.modulate = Color.BLACK
+	for child:TextureRect in $Highlights.get_children():
+		child.modulate = Color(1,1,1,0)
