ad5163408eff471f77d5f3447cc1e472e36faa82
sweatshirtwearingviper <thesweateredviper@gmail.com>
2026-02-01 11:19:29 -0700

    add x camera restrain

--- player_controller/camera/camera.gd
+++ player_controller/camera/camera.gd
@@ -1,10 +1,10 @@
 extends Camera2D
 
 @export var cam_y_offset:float = 0
-@export var max_y:float = -272
-@export var min_y:float = -752
-@export var max_x:float = 0
-@export var min_x:float = 0
+@export var max_y:float = (-540 / 2) - 64
+@export var min_y:float = (-32 * 32) + ((540 / 2) - 64 )
+@export var max_x:float = (256 * 32) - (720 / 2)
+@export var min_x:float = (720 / 2)
 
 var player_position:Vector2 = Vector2.ZERO
 
@@ -14,4 +14,5 @@ func _ready() -> void:
 
 
 func _physics_process(delta: float) -> void:
-	position = Vector2(player_position.x, clampf(player_position.y, min_y + cam_y_offset, max_y  + cam_y_offset))
+	position = Vector2(clampf(player_position.x, min_x, max_x),
+					   clampf(player_position.y, min_y + cam_y_offset, max_y  + cam_y_offset))
